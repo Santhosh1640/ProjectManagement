fee478f62181bc1a11d877726b24538dfb04e280
hemanthsanthosh.s <hemanthsanthosh.s@edgeverve.com>
2024-04-25 14:49:25 +0530

    Project Manaangement

---
+++ src/App.jsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import Sidebar from "./components/Sidebar";
+import SelectedProject from "./components/SelectedProject";
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+    tasks: []
+  });
+
+  function handleAddTask(text) {
+    setProjectsState(prevState => {
+      const taskid = Math.random();
+      const newTask = {
+        text: text,
+        projectid: prevState.selectedProjectId,
+        id: taskid
+      }
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask]
+      }
+    })
+  }
+
+  function handleDeleteTask(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => 
+          task.id !== id)
+      };
+    });
+  }
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    });
+  }
+
+  function handleAddProject(projectData) {
+    //copying all the old existing object using spread operator
+    //and adding new object
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
+
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => 
+          project.id !== prevState.selectedProjectId)
+      };
+    });
+  }
+
+  console.log(projectsState);
+  const selectedProject = projectsState.projects.find(
+    project => project.id === projectsState.selectedProjectId
+  );
+
+  let content = (
+    <SelectedProject 
+      project={selectedProject} 
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}>
+    </SelectedProject>
+  );
+
+  if(projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject}></NewProject>
+  }
+  else if(projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}></NoProjectSelected>
+  }
+
+  
+
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar 
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+        onSelectProject={handleSelectProject}>
+      </Sidebar>
+      {content}
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,13 @@
+import image from '../assets/no-projects.png'
+export default function NoProjectSelected({onStartAddProject}) {
+    return (
+        <div className="mt-24 text-center w-2/3">
+            <img src={image} alt="An empty task list" className="w-16 h-16 object-contain mx-auto"></img>
+            <h2 className="text-xl font-bold text-stone-500 mt-4 my-4">No Project Selected</h2>
+            <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
+            <p>
+                <button onClick={onStartAddProject} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Create new project</button>
+            </p>
+        </div>
+    );
+}
